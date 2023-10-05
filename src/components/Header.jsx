3ce2bf6ecb1d040c6861b276f4f7d47d758d4297
Pappu Dutta <pappudutta279@bitbucket.org>
2023-10-06 00:55:13 +0530

    clean

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { BiChevronDown } from "react-icons/bi";
 import { useDispatch, useSelector } from "react-redux";
