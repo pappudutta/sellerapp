2280d323b0e8cd0464ff93e8eb13d4aa02515cbd
Pappu Dutta <pappudutta279@bitbucket.org>
2023-10-06 16:43:22 +0530

    refactor: header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,30 @@ import { getSearchedData } from "../features/cars/carsSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
-
   const { carsData } = useSelector(state => state.home);
 
   const [searchedCars, setSearchedCars] = useState("");
 
-  const handleChange = event => {
-    event.preventDefault();
+  // on input value change data store to redux store
+  const handleSearchInputChange = event => {
     const value = event.target.value;
+    setSearchedCars(value);
 
     if (value === "") {
       dispatch(getSearchedData(carsData));
     }
-    setSearchedCars(value);
   };
 
-  const onSearch = searchTerm => {
-    setSearchedCars(searchTerm);
+  // handle enter key press
+  const handleKeyPress = event => {
+    if (event.key === "Enter") {
+      return handleSearch();
+    }
+  };
 
+  // handle searches and save search data to redux store new reducer
+  const handleSearch = searchTerm => {
+    setSearchedCars(searchTerm);
     let searchCarsData = [];
 
     if (searchTerm !== "") {
@@ -39,6 +45,36 @@ const Header = () => {
     dispatch(getSearchedData(searchCarsData));
   };
 
+  const renderSearchResults = () => {
+    if (!searchedCars) {
+      return null;
+    }
+
+    const filteredCars = carsData.filter(item => {
+      const searchTerm = searchedCars.toLowerCase();
+      const brandName = item.brand.toLowerCase();
+
+      return (
+        searchTerm &&
+        brandName.startsWith(searchTerm) &&
+        brandName !== searchTerm
+      );
+    });
+    return (
+      <div className="w-full absolute top-9 left-0 bg-white pt-3 rounded-b-xl   ">
+        {filteredCars.map(item => (
+          <div
+            onClick={() => handleSearch(item.brand)}
+            key={item.id}
+            className="cursor-pointer pb-1 w-full px-4"
+          >
+            {item.brand}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="navbar mt-2 mx-auto sticky top-1 mb-10 z-50">
       <div className="w-full flex gap-10 px-5 py-4 drop-shadow-md shadow-gray-50 bg-blue-50 border border-white rounded-2xl">
@@ -49,43 +85,19 @@ const Header = () => {
               name="search"
               id="search"
               value={searchedCars}
-              onChange={e => handleChange(e)}
+              onChange={e => handleSearchInputChange(e)}
+              onKeyUp={e => handleKeyPress(e)}
               placeholder="Search..."
               className="w-full border-none bg-transparent pl-4 text-gray-950 outline-none focus:outline-0"
             />
             <button
-              onClick={() => onSearch(searchedCars)}
+              onClick={() => handleSearch(searchedCars)}
               className="m-2 text-xl rounded p-1 pr-3"
             >
               <FiSearch />
             </button>
           </div>
-          {searchedCars && (
-            <div className="w-full absolute top-9 left-0 bg-white pt-3 rounded-b-xl   ">
-              {carsData
-                .filter(item => {
-                  const searchTerm = searchedCars.toLowerCase();
-                  const brandName = item.brand.toLowerCase();
-
-                  return (
-                    searchTerm &&
-                    brandName.startsWith(searchTerm) &&
-                    brandName !== searchTerm
-                  );
-                })
-                .map(item => (
-                  <div key={item.id} className=" w-full px-4 pb-1">
-                    <div
-                      onClick={() => onSearch(item.brand)}
-                      key={item.id}
-                      className="cursor-pointer pb-1"
-                    >
-                      {item.brand}
-                    </div>
-                  </div>
-                ))}
-            </div>
-          )}
+          {renderSearchResults()}
         </div>
 
         <div className="flex items-center gap-1 cursor-pointer">
